3c686f024ed86d182acceae45f405eca2851c856
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-03 21:59:04 +0700

    Challenge 05

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (arrName, cbSort) =>{
+  let hasil = [];
+  for (let i = 0; i < arrName.length; i++) {
+    hasil.push(i+1+"."+cbSort(arrName)[i]);
+  }
+
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (sortArrName) =>{
+    return sortArrName.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (sortArrName) =>{
+    return sortArrName.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
